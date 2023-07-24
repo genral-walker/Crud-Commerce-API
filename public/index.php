cca6fd832bea0b3f172b021574b3d61e5d1afe5d
Lukman Sanni <lukmansanni60@gmail.com>
2023-07-24 07:41:23 +0100

    add setups

--- public/index.php
+++ public/index.php
@@ -4,3 +4,12 @@ declare(strict_types=1);
 
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+
+/**
+ * First understand the project scopes and user stories
+ * Create the tables
+ * Create the routes and their methods*/
